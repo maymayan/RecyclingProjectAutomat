b72da1ddc2a856df1d52f44ff528c4a19c60e3ef
maymayan <mmaymayan@gmail.com>
2020-02-06 23:45:31 +0300

    added request to backend for adding automat

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import qrcode
+import requests
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
+r = requests.post("http://localhost:8080/rest/automats/addAutomat",
+                  data={"capacity": 100, "isActive": "true", "numberOfBottles": 0,
+                        "location": {"neighborhood": "yenimahalle", "street": "ragip tuzun", "no": "1"}}, )
 
 
 @app.route('/')
@@ -19,9 +24,13 @@ def welcome_page():
     img.save("./static/qrcode.png")
 
     value = "25"
-    return render_template('homepage.html', progress_value=value, progress_style="width:"+value+ "%",
+    return render_template('homepage.html', progress_value=value, progress_style="width:" + value + "%",
                            progress_label="%" + value + " dolu",
                            progress_color="info")
+
+#
+# @app.route('/connected/{username}')
+# def connection_page():
 
 
 if __name__ == '__main__':
